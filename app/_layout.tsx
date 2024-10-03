efa072a27e2c9256b115374238be542c792014f9
juanjozp <juanzpjose136@gmail.com>
2024-10-02 22:20:40 -0500

    update database version

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,32 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native"
-import { useFonts } from "expo-font"
-import { Stack, useSegments, useRouter } from "expo-router"
-import * as SplashScreen from "expo-splash-screen"
-import { useEffect } from "react"
-import "react-native-reanimated"
-import { useColorScheme } from "@/hooks/useColorScheme"
-import { AuthProvider, useAuth } from "@/hooks/useAuth"
-import { SQLiteProvider, type SQLiteDatabase } from "expo-sqlite"
-import { createTables, dropTables, insertDummy } from "@/database/schema"
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack, useSegments, useRouter } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { AuthProvider, useAuth } from "@/hooks/useAuth";
+import { SQLiteProvider, type SQLiteDatabase } from "expo-sqlite";
+import { createTables, dropTables, insertDummy } from "@/database/schema";
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme()
-  const segments = useSegments()
-  const router = useRouter()
-  const { isAuthenticated } = useAuth()
+  const colorScheme = useColorScheme();
+  const segments = useSegments();
+  const router = useRouter();
+  const { isAuthenticated } = useAuth();
 
   useEffect(() => {
-    const isAuthGroup = segments[0] === "(auth)"
+    const isAuthGroup = segments[0] === "(auth)";
     if (!isAuthGroup && !isAuthenticated) {
-      router.replace("/login")
+      router.replace("/login");
     } else if (isAuthGroup && isAuthenticated) {
-      router.replace("/")
+      router.replace("/");
     }
-  }, [isAuthenticated, segments])
+  }, [isAuthenticated, segments]);
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
@@ -32,22 +36,22 @@ function RootLayoutNav() {
         <Stack.Screen name="+not-found" options={{ title: "Oops!" }} />
       </Stack>
     </ThemeProvider>
-  )
+  );
 }
 
 export default function RootLayout() {
   const [isFontsReady] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  })
+  });
 
   useEffect(() => {
     if (isFontsReady) {
-      SplashScreen.hideAsync()
+      SplashScreen.hideAsync();
     }
-  }, [isFontsReady])
+  }, [isFontsReady]);
 
   if (!isFontsReady) {
-    return null
+    return null;
   }
 
   return (
@@ -56,26 +60,26 @@ export default function RootLayout() {
         <RootLayoutNav />
       </AuthProvider>
     </SQLiteProvider>
-  )
+  );
 }
 
 async function migrateDbIfNeeded(db: SQLiteDatabase) {
-  const DATABASE_VERSION = 2
+  const DATABASE_VERSION = 3;
   let { user_version: currentDbVersion } = (await db.getFirstAsync(
     "PRAGMA user_version"
-  )) as { user_version: number }
+  )) as { user_version: number };
 
   if (currentDbVersion >= DATABASE_VERSION) {
-    return
+    return;
   }
 
-  await db.execAsync(dropTables)
-  await db.execAsync(createTables)
-  await db.execAsync(insertDummy)
-  currentDbVersion = 2
+  await db.execAsync(dropTables);
+  await db.execAsync(createTables);
+  await db.execAsync(insertDummy);
+  currentDbVersion = 3;
 
   // if (currentDbVersion === 1) {
   //   Add more migrations
   // }
-  await db.execAsync(`PRAGMA user_version = ${DATABASE_VERSION}`)
+  await db.execAsync(`PRAGMA user_version = ${DATABASE_VERSION}`);
 }
